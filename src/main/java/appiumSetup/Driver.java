1ce74e0be39084a6e5b995dee625f6979c968095
ArtemBerdnik <baren2016@yandex.ru>
2018-11-11 11:53:24 +0300

    started appium hw2

---
+++ src/main/java/appiumSetup/Driver.java
@@ -0,0 +1,83 @@
+package appiumSetup;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.net.URL;
+
+
+public class Driver extends TestProperties {
+    private static AppiumDriver driverSingle = null;
+    private static WebDriverWait waitSingle;
+    private DesiredCapabilities capabilities;
+
+    // Properties to be read
+    private static String AUT; // (mobile) app under testing
+    protected static String SUT; // site under testing
+    private static String TEST_PLATFORM;
+    private static String DRIVER;
+    private static String DEVICE_NAME;
+
+    protected void prepareDriver() throws Exception {
+        capabilities = new DesiredCapabilities();
+        String browserName;
+
+        System.out.println("Properties: " + currentPropertyFile);//Write current property file name to console
+
+        String resourcePath = "./src/main/resources/";
+        String mobileAppName = getProp("aut");
+        AUT = mobileAppName == null ? null : resourcePath + mobileAppName;
+        System.out.println("aut=" + AUT); //Write Current AUT to console
+        String t_sut = getProp("sut");
+        SUT = t_sut == null ? null : "http://" + t_sut;
+        System.out.println("sut=" + SUT); //Write Current SUT to console
+        TEST_PLATFORM = getProp("platform");
+        DRIVER = getProp("driver");
+        DEVICE_NAME = getProp("devicename");
+
+        //Check type of test platform to set proper browserName capability
+        switch (TEST_PLATFORM) {
+            case "Android":
+                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, DEVICE_NAME);
+                browserName = "Chrome";
+                break;
+            case "iOS":
+                browserName = "Safari";
+                break;
+            default:
+                throw new Exception("Unknown mobile platform");
+        }
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, TEST_PLATFORM);
+
+        //Check for test config type to set corresponding capabilities for WEB/NATIVE test
+        //If SUT null and AUT is not null - we deal with NATIVE test
+        if (AUT != null && SUT == null) {
+            File app = new File(AUT);
+            capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+            //And if opposite - we deal with WEB test
+        } else if (SUT != null && AUT == null) {
+            capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, browserName);
+        } else {
+            throw new Exception("Unclear type of mobile app");
+        }
+        // Init driver with new AppiumDriver object
+        //if (driverSingle == null) driverSingle = new AppiumDriver(new URL(DRIVER), capabilities);
+
+        // Set an object to handle timeouts
+        if (waitSingle == null) waitSingle = new WebDriverWait(driver(), 10);
+
+    }
+
+    //Method to access singleton
+    protected AppiumDriver driver() throws Exception {
+        if (driverSingle == null) prepareDriver();
+        return driverSingle;
+    }
+
+    protected WebDriverWait driverWait() {
+        return waitSingle;
+    }
+}
---
+++ src/test/java/mobile/hw2/SimpleNativeTests.java
@@ -0,0 +1,76 @@
+package mobile.hw2;
+
+import appiumSetup.PropertiesFile;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+@Test(groups = "native")
+public class SimpleNativeTests extends Hooks{
+
+    /**
+     * Constructor to pass PropertyFile type into Hooks to run proper prepareDriver() in @BeforeSuite.
+     */
+    protected SimpleNativeTests() {
+        super(PropertiesFile.NATIVE);
+    }
+
+    @Test(description = "Go to the \"Add Contact\" screen and check it content")
+    public void simplestAddContactScreenTest() throws Exception {
+        //Prefix for elements
+        String app_package_name = "com.example.android.contactmanager:id/";
+        By add_btn = By.id(app_package_name + "addContactButton");
+        driver().findElement(add_btn).click();
+
+        //WebElement for Add Contact screen title
+        By add_contact_title = By.id("android:id/title");
+
+        //Check that "Add contact" title is displayed
+        Assert.assertTrue(driver().findElement(add_contact_title).isDisplayed());
+
+        //Assert virtual keyboard appears using helper method
+        //Assert.assertTrue(checkKeyBoardAppears(driver()));
+
+        //WebElements for each title
+        WebElement target_account_title = driver().findElement(By.id("Target Account"));
+        WebElement contact_name_title = driver().findElement(By.id("Contact Name"));
+        WebElement contact_phone_title = driver().findElement(By.id("Contact Phone"));
+        WebElement contact_email_title = driver().findElement(By.id("Contact Email"));
+
+        //List of WebElements for fields titles
+        List<WebElement> desiredTitles = Arrays.asList(target_account_title, contact_name_title,
+                contact_phone_title, contact_email_title);
+        List<String> expectedTitles = Arrays.asList("Target Account", "Contact Name",
+                "Contact Phone", "Contact Email");
+
+        //Iterate through the titles in List<WebElement> to check each title is displayed
+        for (WebElement title : desiredTitles) {
+            Assert.assertTrue(title.isDisplayed());
+        }
+
+        //Assert titles have expected texts
+        for (int i = 0 ; i < expectedTitles.size(); i++){
+            Assert.assertEquals(desiredTitles.get(i).getText(),expectedTitles.get(i));
+        }
+
+        //Find all necessary fields on "Add Contact" screen
+        WebElement target_account_spinner = driver().findElement(By.id(app_package_name + "accountSpinner"));
+        WebElement contact_name_field = driver().findElement(By.id(app_package_name + "contactNameEditText"));
+        WebElement contact_phone_field = driver().findElement(By.id(app_package_name + "contactPhoneEditText"));
+        WebElement contact_email_field = driver().findElement(By.id(app_package_name + "contactEmailEditText"));
+
+        //Assert all expected fields are displayed
+        Assert.assertTrue(target_account_spinner.isDisplayed());
+        Assert.assertTrue(contact_name_field.isDisplayed());
+        Assert.assertTrue(contact_phone_field.isDisplayed());
+        Assert.assertTrue(contact_email_field.isDisplayed());
+
+        System.out.println("Simplest Appium test done");
+    }
+}
